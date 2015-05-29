1fa4672988200424c33a3307bbc170fbbb3494d4
noa malem shinitski <noa.shinitski@smartb.eu>
2015-05-29 16:37:37 +0200

    Update statistics functions

--- plotting.py
+++ plotting.py
@@ -46,33 +46,40 @@ def get_data(station_id, category = 3, start = '19890101', end = '20131231'):
 
     return pd.TimeSeries(selected)
  
-def calculating_means(time_series,resolution='month'): 
+def calculating_yearly_means(time_series,resolution='month'):
     #creates a DataFrame out of time series, rows-resolution, columns - years
     #for resolution='year' returns Series, indices - years
     #possible resolutions: 'dayofyear', 'month', 'year'
-    years = range(time_series.index.year[0], time_series.index.year[-1]) 
-    #years in period 
+    years = range(time_series.index.year[0], time_series.index.year[-1])
+    #years in period
     yearly_resolution_stats = [time_series.xs(str(year)).groupby\
-    (lambda x : operator.attrgetter(resolution)(x)).mean() for year in years]    
+        (lambda x : operator.attrgetter(resolution)(x)).mean() for year in years]
     df2 = pd.concat(yearly_resolution_stats, axis=1, keys = years)
-#to do something here!    
-    if resolution=='year': 
+    #to do something here!
+    if resolution=='year':
         df2=df2.mean().transpose()
-        #flattening data if there is only one dimension
-    return df2    
+    #flattening data if there is only one dimension
+    return df2
+
+def calculating_total_means(time_series,resolution='month'):
+    new_frame=calculating_yearly_means(time_series,resolution)
+    if resolution !='year':
+        new_frame=new_frame.mean(axis=1) #calculating averages
+    return new_frame
 
 def finding_max(data_frame):
-#same as finding_min
+    #same as finding_min
     if type(data_frame)==pd.core.frame.DataFrame:
-         maximums, maximum_idces=data_frame.max(), data_frame.idxmax()
-         maximum,maximum_idx_1=maximums.max(),maximums.idxmax()
-         maximum_idx_2=maximum_idces.loc[maximum_idx_1]
-         return maximum, maximum_idx_1,maximum_idx_2    
+        maximums, maximum_idces=data_frame.max(), data_frame.idxmax()
+        maximum,maximum_idx_1=maximums.max(),maximums.idxmax()
+        maximum_idx_2=maximum_idces.loc[maximum_idx_1]
+        return maximum, maximum_idx_1,maximum_idx_2
     else:
         return data_frame.max(),data_frame.idxmax()
-    
+
+
 def finding_min(data_frame):
-#we pass to this function TimeSeries after preprocessing
+    #we pass to this function TimeSeries after preprocessing
     if type(data_frame)==pd.core.frame.DataFrame:
         #if not averaging, the input is data frame
         #makes things more complicated
@@ -82,31 +89,46 @@ def finding_min(data_frame):
         #finding absolute minimum, it's first index
         minimum_idx_2=minimum_idces.loc[minimum_idx_1]
         #finding it's second index
-        return minimum, minimum_idx_1,minimum_idx_2 
+        return minimum, minimum_idx_1,minimum_idx_2
     else: #if after averaging, the input is series
         return data_frame.min(),data_frame.idxmin()
-                                                
-    
+
+
 def get_statistics(time_series,resolution='month',function=finding_min,average=True):
     #function to find max/min averages/in total
-    new_frame=calculating_means(time_series,resolution)  
-    #data preprocessing
-    if average and resolution!='year':
-        new_frame=new_frame.mean(axis=1) #calculating averages
-    return function(new_frame) 
-    
+    if average:
+        new_frame=calculating_total_means(time_series,resolution)
+    #calculating averages
+    else:
+        new_frame=calculating_yearly_means(time_series,resolution)
+    return function(new_frame)
 
-def plot_means(time_series, resolution='month', number=1):     
+def plot_means(time_series, resolution='month', number=1):
     #takes timeseries as an input, makes resolution-years dataframe
     #returns list of years, list of mean values for one row
-    new_frame=calculating_means(time_series,resolution)
+    new_frame=calculating_yearly_means(time_series,resolution)
     if type(new_frame) == pd.core.frame.DataFrame:
-        ToPlot=new_frame.iloc[number] # to acess one row, 
+        ToPlot=new_frame.iloc[number] # to acess one row,
     else:
-        ToPlot=new_frame #if we are dealing with years 
-    plotting_indices=ToPlot.index.tolist() 
+        ToPlot=new_frame #if we are dealing with years
+    plotting_indices=ToPlot.index.tolist()
     #get years
     return plotting_indices,ToPlot
+
+def compare_statistics(time_series,resolution='dayofweek',slice1=[0,5],slice2=[5,7]):
+    #comparing mean values in slice1 and slice2 with given resolution
+    new_frame=calculating_total_means(time_series,resolution)
+    #calculating total means
+    statistics1=new_frame.iloc[slice1[0]:slice1[1]].mean()
+    statistics2=new_frame.iloc[slice2[0]:slice2[1]].mean()
+    return statistics1, statistics2
+
+def compare_weather(time_series,resolution='dayofweek',slice1=[0,5],slice2=[5,7]):
+    temperature=compare_statistics(time_series.iloc[:,3],resolution,slice1,slice2)
+    wind_speed=compare_statistics(time_series.iloc[:,12],resolution,slice1,slice2)
+    precipitation=compare_statistics(time_series.iloc[:,13],resolution,slice1,slice2)
+    sunshine=compare_statistics(time_series.iloc[:,15],resolution,slice1,slice2)
+    return temperature,wind_speed,precipitation,sunshine
 
 helpstring = """Enter the code of measure(s) you want to obtain.
 The codes for variables:
