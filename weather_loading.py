23d6ab30c475fcef61d5f686300fd48215901b97
Hanna Röhling <hroehling@gmx.net>
2015-06-29 12:43:17 +0200

    changes

--- weather_loading.py
+++ weather_loading.py
@@ -336,7 +336,7 @@ def load_dataframe(Cities_or_IDs, time_from, time_to, filepath = '', matching_st
     
     """
     if not filepath:
-        pass
+        curr_wd = os.getcwd()
     else:
         curr_wd = os.getcwd()
         os.chdir(filepath)
@@ -450,7 +450,7 @@ def load_dataframe(Cities_or_IDs, time_from, time_to, filepath = '', matching_st
             merged_df['Date'] = pd.to_datetime(merged_df['Date'])
             merged_df = merged_df.set_index('Date')
     
-            dict_of_stations[ID] = merged_df 2
+            dict_of_stations[ID] = merged_df 
             
         except MissingDataError:
             print ('There is no data for station '+ID+' or the ID name is missspelled')
