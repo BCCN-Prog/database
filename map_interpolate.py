e201a30af6e0be81f7af37683afe2d6e425b54bd
karaszka <Karo@wlan249-105.extern.hu-berlin.de>
2015-06-29 11:08:35 +0200

    added draft version of map plotting

---
+++ map_interpolate.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+
+###Reading the city list
+fname = 'downloaded_data/DWD_city_list.txt'
+colspecs = [(0, 11),(12, 21),(21, 31),(32, 45),(46,55),(56,65),(66,107),(108,128)]
+df = pd.read_fwf(fname, colspecs=colspecs, header=0)#, index_col=0)
+df = df.drop(df.index[0])
+df = df.drop(df.index[-1])
+
+print(list(df)) #print keys
+#print(df)
+
+s = [1,3,44,161,184,202,211,217,129,132,2,45,56,77,432,66,435,75,87,778]
+stations = [str(i) for i in s]
+
+df2 = df.query("Stations_id in @stations")
+df3 = df[df.Stations_id.isin(stations)]
+
+#df = df3
+print(df3)
+
+###Getting gps positions
+y = df.as_matrix(columns=['geoBreite']).flatten()
+x = df.as_matrix(columns=['geoLaenge']).flatten()
+
+X = np.array([float(i) for i in x])
+Y = np.array([float(i) for i in y])
+
+Nstations = len(X)
+coordinates = np.array([X,Y]).T
+
+
+###Getting values to plot
+Temp = np.random.randint(0, 30, len(X))
+
+
+###Creating meshgrid to plot
+xmax = max(X); xmin = min(X); xl = xmax-xmin
+ymax = max(Y); ymin = min(Y); yl = xmax-xmin
+dt = 0.01
+dx = dy = dt
+
+
+OX = np.arange(xmin-dx,xmax+2*dx,dx)
+OY = np.linspace(ymin-dy,ymax+2*dy,len(OX))
+XX,YY = np.meshgrid(OX,OY)
+
+###Interpolation
+grid_z0 = griddata(coordinates, Temp, (XX, YY), method='linear')
+
+
+###Plotting
+plt.figure(figsize=(10,10))
+#plt.pcolormesh(XX,YY,grid_z0.T)
+
+plt.imshow(grid_z0.T,interpolation='quadric')
+#plt.plot(X,Y,'ko')
+plt.colorbar()
+
+plt.show()
+
--- plotting.py
+++ plotting.py
@@ -84,8 +84,85 @@ def get_statistics(time_series,resolution='month',function=finding_min, average=
     :param average: Boolean
     :return: function(time_series) according to resolution.
     '''
+<<<<<<< Updated upstream
+=======
+    data = pd.read_csv(path, index_col = 2)
+    
+    date_form =  data.index.values.astype(str)
+    for i in range(0, len(date_form)):
+        date_form[i] = date_form[i][:-2]
+        date_form[i] = pd.to_datetime(date_form[i])
+    data.index = date_form
+    
+    #data = data.astype(str)
+    #pd.to_datetime(df.day + df.month + df.year, format="%d%m%Y")
+    
+    data["STATIONS_ID"] = data["STATIONS_ID"].str.replace(' ', '')
+    data["STATIONS_ID"] = data["STATIONS_ID"].convert_objects(convert_numeric=True)
+    
+    return data
+
+def get_data(data, station_id, category = 3, start=None, end=None):
+    """
+    (pandas.Dataframe, int, list) -> (pandas.DataFrame)
+    Returns desired information from the database about requested city and categories.
+    
+    station_id: The code for the requested city/station
+    
+    category: Can be an int or a list of desired variable(s). By default gets the air temperature.
+    
+        The codes for variables:
+        0: Numerical Index
+        1: STATIONS_ID
+        2: QUALITAETS_NIVEAU
+        3: Air Temperature / LUFTTEMPERATUR
+        4: DAMPFDRUCK
+        5: BEDECKUNGSGRAD
+        6: LUFTDRUCK_STATIONSHOEHE
+        7: REL_FEUCHTE
+        8: WINDGESCHWINDIGKEIT
+        9: Max Air Temperature
+        10: Min Air Temperature
+        11: LUFTTEMP_AM_ERDB_MINIMUM (?)
+        12: Max Wind Speed / WINDSPITZE_MAXIMUM
+        13: Precipitation Height / NIEDERSCHLAGSHOEHE (?)
+        14: NIEDERSCHLAGSHOEHE_IND (?)
+        15: Sunshine Duration
+        16: Snow Height
+    """
+    
+    rlv_station = data[data.iloc[:, 1] == station_id]
+    selected = rlv_station.iloc[:, category]
+    selected.index = pd.DatetimeIndex(selected.index) 
+                
+    return selected[start:end]
+ 
+def calculating_means(time_series,resolution='month'): 
+    #creates a DataFrame out of time series, indices-resolution, columns - years
+    years = range(time_series.index.year[0], time_series.index.year[-1]) #period 
+    yearly_resolution_stats = [time_series.xs(str(year)).groupby(lambda x : operator.attrgetter(resolution)(x)).mean() for year in years]    
+    df2 = pd.concat(yearly_resolution_stats, axis=1, keys = years)
+    
+    return df2    
+
+def finding_max(data_frame):#we pass to this function DataFrame either after preprocessing
+                           
+    return data_frame.max(),data_frame.idxmax()
+    
+def finding_min(data_frame):#same as finding_max
+
+    return data_frame.min(),data_frame.idxmin()
+                                                #data_frame.values.min() returns nan
+
+def finding_maximum(data_frame):
+    index = data_frame.unstack()[data_frame.unstack().argsort()].index[-1:]
+    return index
+    
+def get_statistics(time_series,resolution='month',function=finding_min,average=True):
+>>>>>>> Stashed changes
     #function to find max/min averages/in total
     if average:
+<<<<<<< Updated upstream
         new_frame=calculating_total_means(time_series,resolution)
     #calculating averages
     else:
@@ -267,3 +344,60 @@ def main(id, startyear, endyear, measure, resolution, function, average, plottin
         plot_res(data_slice, resolution, startyear, endyear)
 if __name__ == "__main__":
     main()
+=======
+        transformed_frame=transformed_frame.mean(axis=1) 
+    return function(transformed_frame) 
+    #doesn-t work when not averaging
+
+def plot_one_month_means(time_series, resolution='month', month_number=11):     
+    #takes timeseries as an imput, makes month-years dataframe, plots one row of it
+    #now only works with mobths
+    months_years_means=calculating_means(time_series,resolution)
+    ToPlot=months_years_means.iloc[month_number,:] # to acess one row
+    
+    #plotting
+    plt.figure()
+    x =np.arange(len(ToPlot))
+    plt.plot(x,ToPlot,'o')
+    plt.xticks(list(np.arange(len(ToPlot))),months_years_means.columns.values.tolist())
+    plt.xlim(x[0]-0.5,x[-1]+0.5)
+    plt.show()    
+
+Data=load_data('db.txt')
+
+timeSeries=get_data(Data,1)
+
+
+timeSeries=pd.TimeSeries(timeSeries)
+print(timeSeries)
+
+plot_one_month_means(timeSeries)
+print(get_statistics(timeSeries,resolution='month',function=finding_min,average=True))
+'''
+print(Data)
+
+print(finding_maximum(Data))
+'''
+#For now:
+
+#Problem! whene averaging we obtain Series, max,idxmax work
+#Without we have DataFrame, max,idmax doesn-t work
+
+#average=False returns 
+#1937    17.693548
+#1938    16.806452
+#1939    17.393548 ...
+
+#1938     8
+#1939     8
+#1940     7
+
+#average=True returns right thing
+
+#dayofyear attribute returns 1...365? not day-month
+
+#Plotting
+#we can only plot months, also want years and days
+
+##different kinds of plots...
+>>>>>>> Stashed changes
