2ee37f9fa375ab9a4cf15204b0a3791fd99b723e
beckyblythe <tania.asc@gmail.com>
2015-06-29 11:13:54 +0200

    no message

--- plotting.py
+++ plotting.py
@@ -84,85 +84,8 @@ def get_statistics(time_series,resolution='month',function=finding_min, average=
     :param average: Boolean
     :return: function(time_series) according to resolution.
     '''
-<<<<<<< Updated upstream
-=======
-    data = pd.read_csv(path, index_col = 2)
-    
-    date_form =  data.index.values.astype(str)
-    for i in range(0, len(date_form)):
-        date_form[i] = date_form[i][:-2]
-        date_form[i] = pd.to_datetime(date_form[i])
-    data.index = date_form
-    
-    #data = data.astype(str)
-    #pd.to_datetime(df.day + df.month + df.year, format="%d%m%Y")
-    
-    data["STATIONS_ID"] = data["STATIONS_ID"].str.replace(' ', '')
-    data["STATIONS_ID"] = data["STATIONS_ID"].convert_objects(convert_numeric=True)
-    
-    return data
-
-def get_data(data, station_id, category = 3, start=None, end=None):
-    """
-    (pandas.Dataframe, int, list) -> (pandas.DataFrame)
-    Returns desired information from the database about requested city and categories.
-    
-    station_id: The code for the requested city/station
-    
-    category: Can be an int or a list of desired variable(s). By default gets the air temperature.
-    
-        The codes for variables:
-        0: Numerical Index
-        1: STATIONS_ID
-        2: QUALITAETS_NIVEAU
-        3: Air Temperature / LUFTTEMPERATUR
-        4: DAMPFDRUCK
-        5: BEDECKUNGSGRAD
-        6: LUFTDRUCK_STATIONSHOEHE
-        7: REL_FEUCHTE
-        8: WINDGESCHWINDIGKEIT
-        9: Max Air Temperature
-        10: Min Air Temperature
-        11: LUFTTEMP_AM_ERDB_MINIMUM (?)
-        12: Max Wind Speed / WINDSPITZE_MAXIMUM
-        13: Precipitation Height / NIEDERSCHLAGSHOEHE (?)
-        14: NIEDERSCHLAGSHOEHE_IND (?)
-        15: Sunshine Duration
-        16: Snow Height
-    """
-    
-    rlv_station = data[data.iloc[:, 1] == station_id]
-    selected = rlv_station.iloc[:, category]
-    selected.index = pd.DatetimeIndex(selected.index) 
-                
-    return selected[start:end]
- 
-def calculating_means(time_series,resolution='month'): 
-    #creates a DataFrame out of time series, indices-resolution, columns - years
-    years = range(time_series.index.year[0], time_series.index.year[-1]) #period 
-    yearly_resolution_stats = [time_series.xs(str(year)).groupby(lambda x : operator.attrgetter(resolution)(x)).mean() for year in years]    
-    df2 = pd.concat(yearly_resolution_stats, axis=1, keys = years)
-    
-    return df2    
-
-def finding_max(data_frame):#we pass to this function DataFrame either after preprocessing
-                           
-    return data_frame.max(),data_frame.idxmax()
-    
-def finding_min(data_frame):#same as finding_max
-
-    return data_frame.min(),data_frame.idxmin()
-                                                #data_frame.values.min() returns nan
-
-def finding_maximum(data_frame):
-    index = data_frame.unstack()[data_frame.unstack().argsort()].index[-1:]
-    return index
-    
-def get_statistics(time_series,resolution='month',function=finding_min,average=True):
->>>>>>> Stashed changes
     #function to find max/min averages/in total
     if average:
-<<<<<<< Updated upstream
         new_frame=calculating_total_means(time_series,resolution)
     #calculating averages
     else:
@@ -241,39 +164,38 @@ def compare_weather(time_series,resolution='dayofweek',slice1=[0,5],slice2=[5,7]
 
 helpstring = """Enter the code of measure(s) you want to obtain.
 The codes for variables:
-0: Numerical Index
-1: STATIONS_ID
-2: QUALITAETS_NIVEAU
-3: Air Temperature / LUFTTEMPERATUR
-4: DAMPFDRUCK
-5: BEDECKUNGSGRAD
-6: LUFTDRUCK_STATIONSHOEHE
-7: REL_FEUCHTE
-8: WINDGESCHWINDIGKEIT
-9: Max Air Temperature
-10: Min Air Temperature
-11: LUFTTEMP_AM_ERDB_MINIMUM (?)
-12: Max Wind Speed / WINDSPITZE_MAXIMUM
-13: Precipitation Height / NIEDERSCHLAGSHOEHE (?)
-14: NIEDERSCHLAGSHOEHE_IND (?)
-15: Sunshine Duration
-16: Snow Height"""
+0: STATIONS_ID
+1: QUALITAETS_NIVEAU
+2: Air Temperature / LUFTTEMPERATUR
+3: DAMPFDRUCK
+4: BEDECKUNGSGRAD
+5: LUFTDRUCK_STATIONSHOEHE
+6: REL_FEUCHTE
+7: WINDGESCHWINDIGKEIT
+8: Max Air Temperature
+9: Min Air Temperature
+10: LUFTTEMP_AM_ERDB_MINIMUM (?)
+11: Max Wind Speed / WINDSPITZE_MAXIMUM
+12: Precipitation Height / NIEDERSCHLAGSHOEHE (?)
+13: NIEDERSCHLAGSHOEHE_IND (?)
+14: Sunshine Duration
+15: Snow Height"""
 
 @click.command()
-@click.option('--id', type=click.STRING, default = '00131', \
+@click.option('--id', type=click.STRING, default = ['06337','00070'], \
               help="Enter the ID of the station whose data you would like to receive")
-@click.option('--startyear', type=click.INT, default=2005, \
+@click.option('--startyear', type=click.INT, default=1995, \
     help="Enter year that you want to begin your query as INT")
 @click.option("--endyear", type=click.INT, default=2014, \
     help="Enter year that you want to begin your query as INT")
-@click.option("--measure", type=click.INT, default=3, help=helpstring)
+@click.option("--measure", type=click.INT, default=2, help=helpstring)
 @click.option("--resolution", type=click.STRING, default="month", \
     help="Enter the time measure that you want. \n 'dayofyear', 'dayofweek', 'month' or 'year'")
 @click.option("--function", type=click.STRING, default="min", \
     help="Enter the function you want to use. 'min' or 'max'")
 @click.option("--average", type=click.BOOL, default=True, \
     help="Enter if you want the average of the given data")
-@click.option("--plotting", type=click.BOOL, default=False, \
+@click.option("--plotting", type=click.BOOL, default=True, \
     help="Enter 'True' if you want to plot")
 @click.option("--weekend_comparison", type=click.BOOL, default=False, \
     help="Enter 'True' if you want to compare between weekdays and weekend")
@@ -285,16 +207,19 @@ def main(id, startyear, endyear, measure, resolution, function, average, plottin
         id = [id]
 
     func_dict = {'min':finding_min, 'max':finding_max}
-    measures_dict = {4: 'DAMPFDRUCK', 3: 'Air Temperature', 5: 'BEDECKUNGSGRAD', 6: 'LUFTDRUCK_STATIONSHOEHE',\
-                     7: 'REL_FEUCHTE', 8:'WINDGESCHWINDIGKEIT', 9: 'Max Air Temperature', 10: 'Min Air Temperature', \
-                    11: 'LUFTTEMP_AM_ERDB_MINIMUM', 12: 'Max Wind Speed', 13: 'Precipitation Height', \
-                     14: 'IEDERSCHLAGSHOEHE_IND', 15: 'Sunshine Duration', 16: 'now Height'}
+    measures_dict = {3: 'DAMPFDRUCK', 2: 'Air Temperature', 4: 'BEDECKUNGSGRAD', 5: 'LUFTDRUCK_STATIONSHOEHE',\
+                     6: 'REL_FEUCHTE', 7:'WINDGESCHWINDIGKEIT', 8: 'Max Air Temperature', 9: 'Min Air Temperature', \
+                    10: 'LUFTTEMP_AM_ERDB_MINIMUM', 11: 'Max Wind Speed', 12: 'Precipitation Height', \
+                     13: 'IEDERSCHLAGSHOEHE_IND', 14: 'Sunshine Duration', 15: 'now Height'}
 
     data = load_dataframe(id, start, end)
-    for station in data:
-        selected = data[station].iloc[:, measure]
-    data_slice = pd.TimeSeries(selected)
-
+       
+    data_slice=pd.concat([data[station].iloc[:, measure] for station in data],axis=1)
+    print(data_slice)
+    
+    data_slice=data_slice.mean(axis=1)
+    data_slice = pd.TimeSeries(data_slice)
+    
     final_stats = get_statistics(data_slice, resolution, func_dict[function], average)
 
     average_message = ' on average' if average else ''
@@ -344,60 +269,3 @@ def main(id, startyear, endyear, measure, resolution, function, average, plottin
         plot_res(data_slice, resolution, startyear, endyear)
 if __name__ == "__main__":
     main()
-=======
-        transformed_frame=transformed_frame.mean(axis=1) 
-    return function(transformed_frame) 
-    #doesn-t work when not averaging
-
-def plot_one_month_means(time_series, resolution='month', month_number=11):     
-    #takes timeseries as an imput, makes month-years dataframe, plots one row of it
-    #now only works with mobths
-    months_years_means=calculating_means(time_series,resolution)
-    ToPlot=months_years_means.iloc[month_number,:] # to acess one row
-    
-    #plotting
-    plt.figure()
-    x =np.arange(len(ToPlot))
-    plt.plot(x,ToPlot,'o')
-    plt.xticks(list(np.arange(len(ToPlot))),months_years_means.columns.values.tolist())
-    plt.xlim(x[0]-0.5,x[-1]+0.5)
-    plt.show()    
-
-Data=load_data('db.txt')
-
-timeSeries=get_data(Data,1)
-
-
-timeSeries=pd.TimeSeries(timeSeries)
-print(timeSeries)
-
-plot_one_month_means(timeSeries)
-print(get_statistics(timeSeries,resolution='month',function=finding_min,average=True))
-'''
-print(Data)
-
-print(finding_maximum(Data))
-'''
-#For now:
-
-#Problem! whene averaging we obtain Series, max,idxmax work
-#Without we have DataFrame, max,idmax doesn-t work
-
-#average=False returns 
-#1937    17.693548
-#1938    16.806452
-#1939    17.393548 ...
-
-#1938     8
-#1939     8
-#1940     7
-
-#average=True returns right thing
-
-#dayofyear attribute returns 1...365? not day-month
-
-#Plotting
-#we can only plot months, also want years and days
-
-##different kinds of plots...
->>>>>>> Stashed changes
