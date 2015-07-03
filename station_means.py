11972b6f5d996c3b1d05eba5bb23c2ff5cf753aa
name <mail>
2015-07-03 13:44:03 +0200

    heatmap_germany!

--- station_means.py
+++ station_means.py
@@ -1,4 +1,5 @@
 import datetime
+import os.path
 from weather_loading import load_dataframe
 import pandas as pd
 import numpy as np
@@ -6,7 +7,7 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
 
-def read_dwd_city_list(fname='downloaded_data/DWD_city_list.txt'):
+def read_dwd_city_list(fname=os.path.join('downloaded_data', 'DWD_city_list.txt')):
     colspecs = [(0, 11),(12, 21),(21, 31),(32, 45),(46,55),(56,65),(66,107),(108,128)]
     df = pd.read_fwf(fname, colspecs=colspecs, header=0, encoding='Latin-1')
 
@@ -22,13 +23,21 @@ def read_dwd_city_list(fname='downloaded_data/DWD_city_list.txt'):
 
     # Use Stations_ID as index
     df = df.set_index(['Stations_id'])
+    df.sort_index(inplace=True)
 
     return df
 
 
-def station_mean_measure(stations, measure=2,
+def station_mean_measure(stations=None, measure=2,
                          start_date=(datetime.datetime.now()-datetime.timedelta(datetime.datetime.now().weekday(), weeks=1)),
-                         end_date=datetime.datetime.now()):
+                         end_date=datetime.datetime.now(),
+                         fname=os.path.join('downloaded_data', 'DWD_city_list.txt')):
+
+    df_city_list = read_dwd_city_list(fname)
+
+    if stations is None:
+        df = read_dwd_city_list(fname)
+        stations = [int(i) for i in df.index]
 
     # load_dataframe expects a string
     if type(stations[0]) == int:
@@ -43,61 +52,51 @@ def station_mean_measure(stations, measure=2,
 
     df[['Stations_id']] = df[['Stations_id']].astype(int)
     df = df.set_index(['Stations_id'])
+    df[['geoBreite', 'geoLaenge']] = df_city_list.loc[df.index, ['geoBreite', 'geoLaenge']]
+    df.sort_index(inplace=True)
 
     return df
 
 
+def heatmap_germany(stations=None,
+             measure=2,
+             start_date=(datetime.datetime.now()-datetime.timedelta(datetime.datetime.now().weekday(), weeks=1)),
+             end_date=datetime.datetime.now(),
+             fname=os.path.join('downloaded_data', 'DWD_city_list.txt')):
+
+    df = station_mean_measure(stations, measure=measure,
+                              start_date=start_date,
+                              end_date=end_date,
+                              fname=fname)
+    df.dropna()
+
+    X = df['geoBreite'].as_matrix().flatten()
+    Y = df['geoLaenge'].as_matrix().flatten()
+    coordinates = np.array([X, Y]).T
+
+    ###Creating meshgrid to plot
+    xmax = max(X); xmin = min(X)
+    ymax = max(Y); ymin = min(Y)
+    dt = 0.01
+    dx = dy = dt
+
+    OX = np.arange(xmin-dx,xmax+2*dx,dx)
+    OY = np.linspace(ymin-dy,ymax+2*dy,len(OX))
+    XX,YY = np.meshgrid(OX,OY)
+
+    ###Interpolation
+    grid_z0 = griddata(coordinates, df['Time avg.'].as_matrix().flatten(),
+                       (XX, YY), method='nearest')
+
+
+    ###Plotting
+    plt.figure(figsize=(10,10))
+    plt.pcolormesh(XX,YY,grid_z0.T)
+    plt.plot(X,Y,'ko')
+    plt.colorbar()
+
+    plt.show()
+
+
 stations = [71, 161]
-###Getting values to plot
-#Temp = np.random.randint(0, 30, len(X))
-
-
-df = read_dwd_city_list()
-df2 = station_mean_measure(stations)
-
-print(df2)
-
-X = df.loc[df2.index, 'geoBreite'].as_matrix().flatten()
-Y = df.loc[df2.index, 'geoLaenge'].as_matrix().flatten()
-
-Nstations = len(X)
-coordinates = np.array([X, Y]).T
-
-
-'''
-
-def plot_map(stations='All')
-
-	df = read_dwd_city_list()
-	df2 = station_mean_measure(stations)
-
-
-	X = df.loc[df2.index, 'geoBreite'].as_matrix().flatten()
-	Y = df.loc[df2.index, 'geoLaenge'].as_matrix().flatten()
-
-	Nstations = len(X)
-	coordinates = np.array([X, Y]).T
-
-	###Creating meshgrid to plot
-	xmax = max(X); xmin = min(X); xl = xmax-xmin
-	ymax = max(Y); ymin = min(Y); yl = ymax-ymin
-	dt = 0.01
-	dx = dy = dt
-
-	OX = np.arange(xmin-dx,xmax+2*dx,dx)
-	OY = np.linspace(ymin-dy,ymax+2*dy,len(OX))
-	XX,YY = np.meshgrid(OX,OY)
-
-	###Interpolation
-	grid_z0 = griddata(coordinates, Temp, (XX, YY), method='nearest')
-
-
-	###Plotting
-	plt.figure(figsize=(10,10))
-	plt.pcolormesh(XX,YY,grid_z0.T)
-	plt.plot(X,Y,'ko')
-	plt.colorbar()
-
-	plt.show()
-
-'''
+heatmap_germany(stations)
